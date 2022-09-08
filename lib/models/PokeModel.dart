b87787db3fdf4fdf136f24ead6d15ae86fab5f24
Juan Larrain <juanlarra2000@gmail.com>
2022-09-08 16:35:50 -0300

    minor changes

--- lib/models/PokeModel.dart
+++ lib/models/PokeModel.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 
 class Pokemon{
   int id;
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -41,6 +41,8 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
