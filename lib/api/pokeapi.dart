c0cd4ed41cf71dfd07a420fe1e582ac9d3663bc7
Juan Larrain <juanlarra2000@gmail.com>
2022-09-08 13:01:38 -0300

    first commit

---
+++ lib/api/pokeapi.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+
+class PokeApi{
+  static Future<http.Response> getPokemon() => 
+      http.get(Uri.parse("https://pokeapi.co/api/v2/pokemon?limit=250"));
+
+
+}
