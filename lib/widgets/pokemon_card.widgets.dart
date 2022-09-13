5700bea1454d9754e6a3a84d199dd9bad357efda
juanlarrain20 <juanlarra2000@gmail.com>
2022-09-13 18:32:56 -0300

    refactoring

--- lib/widgets/pokemon_card.widgets.dart
+++ lib/widgets/pokemon_card.widgets.dart
@@ -2,3 +2,5 @@ export 'PokemonCardWidgets/pokemon_card.dart';
 export 'PokemonCardWidgets/pokemon_card_background.dart';
 export 'PokemonCardWidgets/pokemon_card_data.dart';
 export 'PokemonCardWidgets/pokemon_grid.dart';
+
+
